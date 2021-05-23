3034b5dacadb087641ea0e8c19e561448580f867
Jonas  Eicher <jonas.eicher.ext@pentasys.de>
2021-05-23 17:37:37 +0200

    docs build

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   // The output goes directly into the spring boot app and is served automatically by tomcat.
   outputDir: '../src/main/resources/public/scs/eichershop',
-  outputDir: '../',
+  outputDir: '../docs',
   assetsDir: 'static',
   devServer: {
     proxy: `http://localhost:${process.env.VUE_APP_SPRING_PORT}`,
